70fd4fe9761f7c7b1436f1905f8f1e502d23e6ad
yuhami <yuhami@protonmail.com>
2021-08-31 13:48:34 +0700

    yu

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function App() {
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<p
+				style={{
+					fontFamily: "sans-serif",
+					fontWeight: "bold",
+					fontSize: "3rem",
+					color: "cornflowerblue",
+				}}
+			>
+				What The OK!
+			</p>
+		</div>
+	);
+}
